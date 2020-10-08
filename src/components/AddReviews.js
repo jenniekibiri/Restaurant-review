99df37bab9bda71f9bf000e9f3d5b61d88e92616
jenniekibiri <josphatje@gmail.com>
2020-10-08 11:16:10 +0300

    clean up the ui

--- src/components/AddReviews.js
+++ src/components/AddReviews.js
@@ -5,11 +5,18 @@ export class AddReviews extends Component {
         return (
             <div>
                   <span  type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className=" np-element ">add review</span>
+                   <div class="dropdown-menu card-body  np-shadow-inverse  mr-3" style={{width: "16rem",
+                       position: 'absolute',
+                        height: '290px',
+                        overflow:'scroll'
+          
 
-                   <div class="dropdown-menu card-body np-shadow-inverse " style={{width: "18rem"}} aria-labelledby="dropdownMenuButton1">
+        
+
+          }} aria-labelledby="dropdownMenuButton1">
  
-     <div class="  np-element">
-        <form>
+     <div class=" ">
+        <form >
           <div class="form-group">
            
             <input type="text" class="form-control " placeholder="full name" id="recipient-name"/>
@@ -23,11 +30,12 @@ export class AddReviews extends Component {
             <textarea class="form-control" id="message-text"></textarea>
           </div>
         </form>
+            <div class="row  modal-footer">
+        <button type="button" class="btn  np-element np-shadow bg-danger np-bg-warn text-white" data-dismiss="modal ">Close</button>
+        <button type="button" class="btn  np-element np-shadow  bg-success  text-white ">Send review</button>
       </div>
-      <div class="modal-footer ">
-        <button type="button" class="btn np-element text-white" data-dismiss="modal ">Close</button>
-        <button type="button" class="btn np-element text-white ">Send review</button>
       </div>
+   
   </div>
 
             </div>
