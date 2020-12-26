136768dd1c4498b359277fc737fa02ccb1afc4d2
jenniekibiri <josphatje@gmail.com>
2020-12-26 15:26:28 +0300

    clean up the Ui

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -9,29 +9,26 @@ const mapCenter = {
   lng: 36.817786,
 };
 
-var icon = {
-  url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
-};
+
 const mapStyles = {
   height: "100vh",
   width: "93%",
 };
 export class MapContainer extends Component {
   state = {
-    showingInfoWindow: false, 
-    activeMarker: {}, 
-    selectedPlace: {}, 
-    mapCenter:mapCenter,
-    currentPosition:{}
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    mapCenter: mapCenter,
+    currentPosition: {},
   };
-  onMarkerClick = (props, marker) =>{
- 
+  onMarkerClick = (props, marker) => {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true,
     });
-  }
+  };
   onMapClicked = (props) => {
     if (this.state.showingInfoWindow) {
       this.setState({
@@ -40,21 +37,18 @@ export class MapContainer extends Component {
       });
     }
   };
-    componentDidMount() {
-      
-   navigator.geolocation.getCurrentPosition((position)=> {
- this.setState({
-   currentPosition:{
-     lat:position.coords.latitude,
-     lng:position.coords.longitude
-   }
- })
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        currentPosition: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+      });
     });
- 
- 
   }
   render() {
-    console.log(this.state.currentPosition)
+    console.log(this.state.currentPosition);
     return (
       <div
         style={{ height: "10vh", width: "100%" }}
@@ -67,34 +61,35 @@ export class MapContainer extends Component {
           initialCenter={this.state.mapCenter}
           onClick={this.onMapClicked}
         >
-            <Marker
-             
-              name={"current location"}
-              onClick={this.onMarkerClick}
+          <Marker
+            name={"current location"}
+            title={"current location"}
+            // animation={this.props.google.maps.Animation.BOUNCE}
+            icon={{
+              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+              scaledSize: new window.google.maps.Size(50, 50),
+            }}
             position={this.state.currentPosition}
-            />
-          {places.map((place,i) => (
+          />
+          {places.map((place, i) => (
             <Marker
               key={i}
               name={place.restaurantName}
               onClick={this.onMarkerClick}
               position={{ lat: place.lat, lng: place.long }}
             />
-            
           ))}
 
-         {
-           this.state.activeMarker&&(
-              <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-          >
-            <div>
-              <h6>{this.state.selectedPlace.name}</h6>
-            </div>
-          </InfoWindow>
-           )
-         }
+          {this.state.activeMarker && (
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+            >
+              <div>
+                <h6>{this.state.selectedPlace.name}</h6>
+              </div>
+            </InfoWindow>
+          )}
         </Map>
       </div>
     );
